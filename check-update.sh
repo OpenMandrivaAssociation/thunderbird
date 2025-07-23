5656af66725a2089ba9b996247e23f9b9725978d
Bernhard Rosenkränzer <bero@lindev.ch>
2025-07-23 23:57:27 +0200

    Make auto-update great again

--- check-update.sh
+++ check-update.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl https://www.thunderbird.net/en-US/ 2>&1 |grep download.mozilla.org |grep os=linux64 |head -n1 |sed -e 's,.*product=thunderbird-,,;s,-.*,,'
+curl -s https://ftp.mozilla.org/pub/thunderbird/releases/ |grep /pub/thunderbird/releases |sed -e 's,.*/">,,;s,/.*,,' |grep '^[0-9.]*$' |sort -V |tail -n1
